01ed1cfc30e2bba9673216e43963a3dfeee28418
Jorge Paredes <jorge.paredes@italtel-partners.com>
2022-04-18 15:52:58 -0500

    subiendo la estructura

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="language" content="es"/>
+
+        <title>Fazil</title>
+        <meta name="description" content="Fazil el mejor servicio de delivery on demand. Del local a la puerta de tu casa en minutos. Tercerizar tu delivery y olvidate de los problemas." />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="css/estructura.css" />
+    </head>
+
+    <body>
+
+        <main>
+
+
+          
+        </main>
+
+    </body>
+
+    <footer class="ttl-landing-footer">
+
+        <div class="container">
+
+            <section class="ttl-landing-footer-container">
+
+                <div class="ttl-landing-footer-superior">
+
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                            <div class="ttl-landing-footer-superior-titulo-container">
+                                <p class="ttl-landing-footer-superior-titulo color-white">Suscríbete a nuestro newsletter</p>
+                            </div>
+                        </div>
+                        
+                        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+                            <div class="ttl-landing-footer-superior-descripcion-container">
+                                <p class="ttl-landing-footer-superior-descripcion color-semi-white">Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries took a galley of type and scrambled it to make a type specimen book.</p>
+                            </div>  
+                        </div>
+                            
+                    </div>
+
+                </div>
+
+                <div class="ttl-landing-footer-medio position-relative">
+
+                    <div class="row">
+
+                        <div class="col-4">
+                    
+                            <div class="ttl-landing-footer-medio-informacion-empresa">
+                                <p class="informacion-empresa-logo">
+                                    <img class="img-fondo width-100" src="imgs/footer_logo.png">
+                                </p>
+                                
+                                <ul class="informacion-empresa-items">
+                                    <li class="color-semi-white">
+                                        <i class="fa fa-map-marker color-white" aria-hidden="true"></i>
+                                        <span>Parcela H(Lado del Centro) <br>exposiciones Jockey Club) <br> Surco. Ubicaciones en Waze.</span>
+                                    </li>
+                                    <li class="color-semi-white">
+                                        <i class="fa fa-envelope color-white" aria-hidden="true"></i>
+                                        <span>Lorem ipsum is simpty</span>
+                                    </li> 
+                                </ul>
+                            </div>
+
+                        </div>
+
+                        <div class="col-3">
+
+                            <div class="ttl-landing-footer-medio-paginas">
+                                <ul class="paginas-items">
+                                    <li><a href="#">Misión</a></li>
+                                    <li><a href="#">Beneficios</a></li>   
+                                    <li><a href="#">Sellers</a></li>
+                                    <li><a href="#">Coberturas</a></li>
+                                    <li><a href="#">Emprendedores</a></li>
+                                    <li><a href="#">Testimonios</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                        <div class="col-5">
+
+                            <div class="ttl-landing-footer-medio-clientes">
+                                <ul class="medio-clientes-items">
+                                    <li><a href="#">Atención al Cliente</a></li>
+                                    <li><a href="#">Política de privacidad</a></li>   
+                                    <li><a href="#">Contáctanos</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <img class="ttl-landing-footer-medio-delivery" src="imgs/icon_moto.png" />
+                    
+                </div>
+
+            </section>
+
+        </div>
+
+    </footer>
+
+</html>
+
+<script>
+
+ 
+    
+
+</script>
