fff059654140214d9ffd629a144c2b12d6ae0e8d
Jorge Paredes <jorge.paredes@italtel-partners.com>
2022-04-19 17:37:58 -0500

    subiendo secciones banners, mision y marcas

--- index.php
+++ index.php
@@ -22,6 +22,89 @@
 
         <main>
 
+            <section class="ttl-landing-guidelines-banner-principal position-relative">
+
+                <div class="container">
+              
+                  <picture>
+                    <source media="(min-width:992px)" srcset="imgs/banner_main.png">
+                    <source media="(min-width:768px)" srcset="imgs/banner_main_movil.png">
+                    <source media="(min-width:320px)" srcset="imgs/banner_main_movil.png">
+                    <img class="img-fondo width-100" src="imgs/banner_main.png">
+                  </picture> 
+
+                </div>
+
+            </section>
+
+            <section class="ttl-landing-mision-rol container">
+
+                <div class="container">
+
+                    <div class="ttl-landing-mision">
+
+                        <div class="row">
+
+                            <div class="col-3 text-right align-items-center">
+                                <img src="imgs/item_01.png" />
+                            </div>
+                                
+                            <div class="col-8 align-items-center display-flex">
+                                <p>Misión: Hacerle la vida más fácil a nuestros clientes como centro de todo lo que hacemos.</p>
+                            </div>
+                                    
+                        </div>
+
+                    </div>
+
+                    <div class="ttl-landing-rol">
+
+                        <div class="row">
+
+                            <div class="col-3 text-right align-items-center">
+                                <img src="imgs/item_02.png" />
+                            </div>
+                                
+                            <div class="col-8 align-items-center display-flex">
+                                <p>Rol: Facilitar a las personas la necesidad de <span>hacer compras del supermercado</span></p>
+                            </div>
+                                    
+                        </div>
+                        
+                    </div>
+                  
+                </div>
+
+            </section>
+
+            <section class="ttl-landing-marcas">
+
+                <div class="container">
+              
+                  <picture>
+                    <source media="(min-width:992px)" srcset="imgs/banner_marcas.png">
+                    <source media="(min-width:768px)" srcset="imgs/banner_marcas.png">
+                    <source media="(min-width:320px)" srcset="imgs/banner_marcas.png">
+                    <img class="img-fondo width-100" src="imgs/banner_marcas.png">
+                  </picture> 
+
+                </div>
+
+            </section>
+
+            <div class="container">
+
+                <section class="ttl-landing-covertura">
+                  
+                    <p class="ttl-landing-covertura-titulo text-center">Conoce las zonas de <span>cobertura:</span></p>
+
+                    <div class="ttl-landing-covertura-mapa">
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1KQ4qbayXSR6e2TQNCNPk7Fs0_nw5qiaz" class="coberturamap_prov" frameborder="0" width="80%" height="420px"></iframe>
+                    </div>
+
+                </section>
+
+            </div>
 
           
         </main>
@@ -110,6 +193,24 @@
 
                     <img class="ttl-landing-footer-medio-delivery" src="imgs/icon_moto.png" />
                     
+                </div>
+
+                <div class="ttl-landing-footer-inferior">
+                    <p class="ttl-landing-footer-inferior-derechos color-semi-white">YOY 2022. Todos los derechos reservados - Diseño Web por <a href="#" class="color-white">McCANN</a></p>
+                    <div class="ttl-landing-footer-inferior-redes-sociales align-self-center">
+                        <ul class="redes-sociales-items">
+                            <li><a href="#"><i class="fa fa-facebook fa-lg color-white" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter fa-lg color-white" aria-hidden="true"></i></a></li>   
+                            <li>
+                                <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok color-white" viewBox="0 0 16 16">
+                                        <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"/>
+                                    </svg>
+                                </a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-instagram fa-lg color-white" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
 
             </section>
